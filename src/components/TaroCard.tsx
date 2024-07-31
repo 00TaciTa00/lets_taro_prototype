396a3455c3577167254866ed0146c05fbd576093
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-07-31 19:43:12 +0900

    feature : 카드 펼치기 기능 구현 (#11)

--- src/components/TaroCard.tsx
+++ src/components/TaroCard.tsx
@@ -6,17 +6,27 @@ import styles from "@/styles/TaroCard.module.css";
 interface TaroCardProps {
   name: string; // 카드의 이름
   foreImage: string; // 카드 앞면 이미지 URL
+  isFlipAble?: boolean; // 뒤집을 수 있는 지
+  isHoverAble?: boolean; // 호버하면 카드가 커지는 지
   onClick?: () => void; // 카드 클릭 시 호출되는 함수
 }
 
 const backImage =
-  "https://images.unsplash.com/photo-1517196084897-498e0abd7c2d?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  "https://images.unsplash.com/photo-1577084381380-3b9ea4153664?q=80&w=2612&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-const TaroCard: React.FC<TaroCardProps> = ({ name, foreImage, onClick }) => {
+const TaroCard: React.FC<TaroCardProps> = ({
+  name,
+  foreImage,
+  isFlipAble = false,
+  isHoverAble = false,
+  onClick,
+}) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = () => {
-    setIsFlipped(!isFlipped);
+    if (isFlipAble) {
+      setIsFlipped(!isFlipped);
+    }
     if (onClick) onClick();
   };
 
@@ -32,7 +42,7 @@ const TaroCard: React.FC<TaroCardProps> = ({ name, foreImage, onClick }) => {
             alt={name}
             width={200}
             height={300}
-            className={styles.image}
+            className={`${styles.image} ${isHoverAble ? styles.scale : ""}`}
           />
           <div className={styles.info}>
             <h2 className={styles.title}>{name}</h2>
@@ -44,7 +54,7 @@ const TaroCard: React.FC<TaroCardProps> = ({ name, foreImage, onClick }) => {
             alt={name}
             width={200}
             height={300}
-            className={styles.image}
+            className={`${styles.image} ${isHoverAble ? styles.scale : ""}`}
           />
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,29 @@ const foreImage =
   "https://images.unsplash.com/photo-1498612753354-772a30629934?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 export default function Home() {
-  const [cards, setCards] = useState<number[]>([]);
+  const [cards, setCards] = useState<number[]>(
+    Array.from({ length: 78 }, (_, i) => i)
+  );
+  const [isSpread, setIsSpread] = useState<boolean>(false);
+  const [isShuffling, setIsShuffling] = useState<boolean>(false);
 
-  const shuffleCards = () => {
-    const arr = Array.from({ length: 78 }, (_, i) => i);
-    for (let i = arr.length - 1; i > 0; i--) {
+  const startShuffling = async () => {
+    setIsShuffling(true);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+  };
+
+  const shuffleDeck = () => {
+    startShuffling().then(() => setIsShuffling(false));
+    const arr = [...cards];
+    for (let i = cards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     setCards(arr);
+  };
+
+  const spreadDeck = () => {
+    setIsSpread(!isSpread);
   };
 
   return (
@@ -28,14 +42,120 @@ export default function Home() {
       </Head>
       <main>
         <div className={styles.div}>
-          <TaroCard name="샘플" foreImage={foreImage} />
+          <TaroCard name="샘플" foreImage={foreImage} isFlipAble />
         </div>
-        <button onClick={shuffleCards}>섞기</button>
-        <button>펼치기</button>
         <div>
-          {cards.map((card) => (
-            <span key={card}>{card},</span>
-          ))}
+          <button onClick={shuffleDeck}>
+            섞기 : {isShuffling ? "true" : "false"}
+          </button>
+          <div>
+            {cards.map((card) => (
+              <span key={card}>{card},</span>
+            ))}
+          </div>
+        </div>
+        <div style={{ display: "flex", gap: "8px" }}>
+          {/*
+          <div
+            className={styles.div}
+            style={{
+              flexGrow: "0",
+              position: "relative",
+              width: "400px",
+              height: "350px",
+            }}
+          >
+            {cards.map((card, index) => (
+              <div
+                key={card}
+                className={styles.deck}
+                style={{
+                  transition: "transform 0.3s",
+                  zIndex: cards.length - index,
+                  transform: `translate(${index / 2}px, ${index / 5}px)`,
+                }}
+              >
+                <TaroCard
+                  name={card.toString()}
+                  foreImage={foreImage}
+                  onClick={() => console.log(`Card ${card} clicked`)}
+                />
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.div}
+            style={{
+              flexGrow: "1",
+              position: "relative",
+              width: "100%",
+              height: "350px",
+            }}
+          >
+            {cards.map((card, index) => (
+              <div
+                key={card}
+                className={styles.deck}
+                style={{
+                  transition: "transform 0.3s",
+                  zIndex: cards.length - index,
+                  transform: `translate(${index * 10}px)`,
+                }}
+              >
+                <TaroCard
+                  name={card.toString()}
+                  foreImage={foreImage}
+                  isFlipAble
+                  onClick={() => console.log(`Card ${card} clicked`)}
+                />
+              </div>
+            ))}
+          </div> */}
+        </div>
+        <button onClick={spreadDeck}>{!isSpread ? "펼치기" : "겹치기"}</button>
+        <div
+          className={styles.div}
+          style={{
+            flexGrow: "0",
+            width: "100%",
+            height: "350px",
+            position: "relative",
+            overflow: "hidden",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "relative",
+            }}
+          >
+            {cards.map((card, index) => (
+              <div
+                key={card}
+                className={styles.deck}
+                style={{
+                  top: "0",
+                  transition: "transform 1s",
+                  zIndex: cards.length - index,
+                  transform: !isSpread
+                    ? isShuffling
+                      ? `translate(${index / 2}px, ${index / 5}px)`
+                      : `translate(0)`
+                    : `translate(${(-39 + index) * 11}px)`,
+                }}
+              >
+                <TaroCard
+                  name={card.toString()}
+                  foreImage={foreImage}
+                  isHoverAble={isSpread ? true : false}
+                  onClick={() => console.log(`Card ${card} clicked`)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
