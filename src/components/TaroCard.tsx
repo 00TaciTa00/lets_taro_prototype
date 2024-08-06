6498cfe67377cd223ed2393713c85847599edb8a
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-08-06 17:11:31 +0900

    Feat : 레이아웃 정리 및 카드 지정 기능 추가 (#12)
    * feat : 타로 카드 객체 생성 (#8)
    
    * feature : 카드 뒷면 생성 및 뒤집기 기능 적용 (#9)
    
    * feature : 카드 펼치기 기능 구현 (#11)
    
    * feature : Deck 컴포넌트 생성 완료
    
    * 덱 삭제, 임시 저장
    
    * feature : 카드 놓는 자리 생성 및 크기 조절
    
    * feature : 선택된 카드의 개수에 따라 하단 카드 영역 수 동적으로 변경되는 기능 추가
    
    * feature : 하단 카드 영역에 카드 번호 및 카드 객체 지정

---
+++ src/components/TaroCard.tsx
@@ -0,0 +1,63 @@
+// components/TaroCard.tsx
+import { useState } from "react";
+import Image from "next/image";
+import styles from "@/styles/TaroCard.module.css";
+
+interface TaroCardProps {
+  name: string; // 카드의 이름
+  index: number;
+  foreImage: string; // 카드 앞면 이미지 URL
+  isFlipAble?: boolean; // 뒤집을 수 있는 지
+  isHoverAble?: boolean; // 호버하면 카드가 커지는 지
+  onSelect?: () => void; // 카드 클릭 시 호출되는 함수
+}
+
+const backImage =
+  "https://images.unsplash.com/photo-1577084381380-3b9ea4153664?q=80&w=2612&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+const TaroCard: React.FC<TaroCardProps> = ({
+  name,
+  foreImage,
+  isFlipAble = false,
+  isHoverAble = false,
+  onSelect,
+}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    if (isFlipAble) {
+      setIsFlipped(!isFlipped);
+    }
+  };
+
+  return (
+    <div
+      className={`${styles.card} ${isFlipped && styles.flipped}`}
+      onClick={handleFlip}
+    >
+      <div className={styles.card_inner}>
+        <div className={styles.card_front}>
+          <Image
+            src={foreImage}
+            alt={name}
+            width={150}
+            height={275}
+            className={`${styles.image} ${isHoverAble ? styles.hovered : ""}`}
+          />
+          <h2 className={styles.name}>{name}</h2>
+        </div>
+        <div className={styles.card_back}>
+          <Image
+            src={backImage}
+            alt={name}
+            width={150}
+            height={275}
+            className={`${styles.image} ${isHoverAble ? styles.hovered : ""}`}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaroCard;
