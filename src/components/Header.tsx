8e8c80b43d86dfd9e1a0d744b108ec56b4327872
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-08-21 16:35:45 +0900

    feature : dnd 적용 (#14)
    * feature : DnD 적용
    
    * feature : 임시저장
    
    * feature : react-draggable 적용
    
    * fix : CardPlace.tsx remove
    
    * fix : PlaceSection.tsx remove

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/styles/Header.module.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Let&apos;s Taro!</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/testpage/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout";
+
+export default function TestPage() {
+  return (
+    <Layout>
+      <div>testpage</div>
+    </Layout>
+  );
+}
